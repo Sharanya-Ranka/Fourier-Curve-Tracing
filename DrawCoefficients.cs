840d37c8ce739846d6f2df33c8ff7f79b8a8f000
Sharanya-Ranka <64748456+Sharanya-Ranka@users.noreply.github.com>
2020-11-30 23:13:54 +0530

    Update DrawCoefficients.cs
    Cleaned up code

--- DrawCoefficients.cs
+++ DrawCoefficients.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
@@ -12,53 +12,39 @@ public class DrawCoefficients : MonoBehaviour
     public GameObject ArrowPrefab;
     public List<GameObject> availableObjects = new List<GameObject>();
     public List<GameObject> availableArrows = new List<GameObject>();
-    //private Func<double,double[]> fourier_series;
 
-    //public LineRenderer line;
-
-    double time=0.0d;
+    double time=0.0d, time_delta = 0.001d;
     private static double pi=3.14159265;
+
+    private bool visibility = true;
 
     private Vector3 temp_position;
     // Start is called before the first frame update
     public void CalculateFourierSeries(Func<double,double[]> function_to_transform, int max_frequency)
     {
-        temp_position=transform.position;
+        /*
+        Gets the coefficients of the fourier series of function_to_transform.
+        Is used in making the arrows that track the cumulative movement of each frequency.
+        */
 
         coefficients=FourierSeries.fft_GetFourierCoefficients(function_to_transform,max_frequency);
         
+        //Instantiating required GameObjects
 
         for(int i=0;i<(coefficients[0].Count+1)/2;i++)
         {
             availableObjects.Add(Instantiate(Prefab,new Vector3(0, 0, 0), Quaternion.identity));
             availableArrows.Add(Instantiate(ArrowPrefab, new Vector3(0, 0, 0), Quaternion.identity));
         }
-
-        foreach(Dictionary<int,Complex> dk in coefficients)
-        {
-            //Print all the coefficients for each dimension
-            Debug.Log("For dimension");
-            foreach(KeyValuePair<int,Complex> c in dk)
-            {
-                Debug.Log(c.Key+": "+c.Value.Real+"+ i"+c.Value.Imaginary);
-            }
-            Debug.Log("Hi\n");
-        }
-
-        // line=GetComponent<LineRenderer>();
-        // line.positionCount=0;
-        //line.width=0.04f;
-        // Debug.Log(coefficients);
     }
 
-    // Update is called once per frame
-    public void AUpdate()
+    public void FunctionUpdate()
     {
-        time+=0.001;
-        // double[] pos=fourier_series(time);
-        // Debug.Log(pos[0]+" "+pos[1]+" "+pos[2]);
+        time+= time_delta;
+
         Vector3 prev_pos = new Vector3(0,0,0);
         int component_count = (coefficients[0].Count+1)/2;
+
         for(int go = 0; go < component_count; go++)
         {
             Vector3 pos = new Vector3(0,0,0);
@@ -72,45 +58,50 @@ public class DrawCoefficients : MonoBehaviour
                     Complex multiplier_conj=Complex.FromPolarCoordinates(1.0d,-2*pi*go*time);
                     t+=multiplier_conj*coefficients[dim][-go]*2;
                 }
-
-                
-                Debug.Log("Finding t : Dimension "+dim+" t is "+t);
                 pos[dim] = (float)t.Real;
             }
-            
-            //pos[0]+= 20*go;
+            //Fixing position, rotation and scale of the arrow corresponding to frequency 'go'
             availableArrows[go].transform.position = prev_pos;
             float scaleby_float = Vector3.Distance(prev_pos, prev_pos+pos);
             Vector3 scaleby = new Vector3(scaleby_float, scaleby_float, scaleby_float);
             availableArrows[go].transform.localScale = scaleby;
             availableArrows[go].transform.LookAt(prev_pos+pos);
-            if(go == component_count-1)
-                availableObjects[go].transform.position= prev_pos+pos;
-            prev_pos +=pos;
 
-            
-            if(go == component_count-1)
-                UpdateLineRenderer(go, prev_pos);
+            prev_pos += pos;
         }
 
-        // temp_position.x=(float)pos[0];
-        // temp_position.y=(float)pos[1];
-        // temp_position.z=(float)pos[2];
-
-        
-        // transform.position=temp_position;
-
-        // UpdateLineRenderer();
-        
+        // Updating the position and line renderer of the last coefficient 
+        // Used to find out which curve the set of arrows are tracing
+        availableObjects[component_count-1].transform.position= prev_pos;
+        UpdateLineRenderer(component_count-1, prev_pos);
     }
 
     public void UpdateLineRenderer(int game_obj_ind, Vector3 pos)
     {
+        /*
+        Updates line renderer of the particular game object.
+        Adds position pos to the list
+        */
         LineRenderer line = availableObjects[game_obj_ind].GetComponent<LineRenderer>();
-        if(time<=1.01)
+        if(time<=1 + time_delta)
         {
             line.positionCount++;
             line.SetPosition(line.positionCount-1,pos);
+        }
+    }
+
+    public void ToggleVisibility()
+    {
+        visibility = ! visibility;
+
+        for(int i = 0;i<availableObjects.Count; i++)
+        {
+            availableObjects[i].GetComponent<Renderer>().enabled = visibility;
+            availableObjects[i].GetComponent<LineRenderer>().enabled = visibility;
+        }
+        for(int i = 0; i<availableArrows.Count; i++)
+        {
+            availableArrows[i].GetComponent<Renderer>().enabled = visibility;
         }
     }
 }
